16777c25f3b49e0bd76728e641bcc97f698e54a4
patrickryan <theyoungsoul@gmail.com>
2022-07-15 12:26:38 -0300

    chapter 9

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,9 +5,17 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ("age",)
+        fields = (
+            "username",
+            "email",
+            "age"
+        )
         
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
-        fields = UserChangeForm.Meta.fields
+        fields = (
+            "username",
+            "email",
+            "age"
+        )
